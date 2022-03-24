2972451468fa77238e1a42a07372ef0f49ede177
dxhuii <dxhuii@qq.com>
2022-03-24 14:10:10 +0800

    修改vite配置

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,25 @@ import uni from '@dcloudio/vite-plugin-uni'
 import Unocss from 'unocss/vite'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [uni(), Unocss()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src')
+export default defineConfig(({ command }) => {
+  return {
+    plugins: [uni(), Unocss()],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, 'src')
+      }
+    },
+    build: {
+      terserOptions: {
+        /**
+         * command 用来判断环境
+         */
+        compress: {
+          drop_console: command !== 'serve',
+          // 默认是true
+          drop_debugger: command !== 'serve'
+        }
+      }
     }
   }
 })
